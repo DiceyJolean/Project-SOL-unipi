6d888183ae46fa2193f7c9596e8ef17590b5336f
DiceyJolean <87235846+DiceyJolean@users.noreply.github.com>
2022-01-07 15:26:20 +0100

    Added exception

--- storage.h
+++ storage.h
@@ -1,7 +1,7 @@
+// Erica Pistolesi 518169
+
 #ifndef STORAGE_H
 #define STORAGE_H
-
-#define _POSIX_C_SOURCE 200809L
 
 #include <pthread.h>
 #include <sys/select.h>
@@ -12,13 +12,9 @@
 #include "icl_hash.h"
 // #include "serverutility.h"
 
-#define MAX_BUCKETS 100;
-#define FATAL 35;
-
-// TODO
-#define MAX_CHAR_LENGHT 1024
-static int max_storage = 1028;
-static int max_file = 10;
+#ifndef EFATAL
+#define EFATAL -1
+#endif
 
 // -------------------- Struttura dati e attributi per la cache --------------------
 
@@ -32,10 +28,12 @@ typedef struct storage{
     pthread_mutex_t lock_cache;
     pthread_cond_t cond_lettori_cache;
     pthread_cond_t cond_scrittura_cache;
-    int actual_size;
-    int max_used_size;
-    int actual_file;
-    int max_used_file;
+    int actual_size; // Dimensione attuale dello storage
+    int max_used_size; // Dimensione massima raggiunta dallo storage
+    int actual_file; // Numero di file attualmente presenti nello storage
+    int max_used_file; // Numero di file massimo raggiunto nello storage
+    int max_size; // Limite massimo per la dimensione totale dello storage
+    int max_file; // Numero massimo di file contenuti nello storage
 } storage_t;
 
 // -------------------- Struttura dati e attributi per i singoli file --------------------
@@ -47,7 +45,7 @@ typedef struct file{
     fd_set openedBy;    // Insieme dei fd dei client da cui è stato aperto
     int lockedBy;       // FD del detentore della lock
     fd_set lockWait; // Insieme dei fd dei client in attesa della lock
-    int maxfd; // Massimo fd dell'insime lockwaiters
+    int maxfd; // Massimo fd dell'insime lockwaiters, corrisponde al prossimo client che otterrà la lock
     int okUpload; // Flag che indica se è consentina la writeFile
 
     int scrittori_file_attivi;
@@ -61,13 +59,31 @@ typedef struct file{
 } file_t;
 
 /**
+ * @brief   Per ogni file presente nello storage aperto o bloccato dal client indicato dal file descriptor fd,
+ *          elimina il client dall'insieme dei client che avevano aperto tali file e sblocca tutti i file da 
+ *          lui bloccati, restituisce inoltre la coda dei client in attesa di ottenere la lock su quegli stessi file
+ * 
+ * @param cache Puntatore allo storage
+ * @param fd File descriptor del client da eliminare dallo storage
+ * @param lockWaiters Coda dei file descriptor dei client che erano in attesa della lock su file bloccati dal client da eliminare
+ * 
+ * @return 0 in caso di successo, -1 altrimenti
+ */
+int closeFd(storage_t* cache, int fd, intqueue_t* lockWaiters);
+
+void printCache(storage_t* cache);
+
+/**
  * @brief Inizializza uno storage
  * 
- * @param nbuck Numero di bucket che conterrà la tabella hash nello storage
+ * @param max_file Numero massimo di file contenuti nello storage
+ * @param max_size Capacità massima dello storage
+ * 
+ * @exception ENOMEM se fallisce l'allocazione dei campi dello storage
  * 
  * @return il puntatore allo storage appena create, NULL in caso di errore
  */
-storage_t* S_createStorage(int nbuck);
+storage_t* S_createStorage(int max_file, int max_size);
 
 /**
  * @brief Elimina uno storage e libera la memoria
@@ -87,6 +103,9 @@ int deleteStorage(storage_t* storage);
  * @param idClient FD del client che richiede la lock
  * @param enqueued Flag che indica se il client è in attesa della lock
  * 
+ * @exception   EINVAL se pathname è NULL, se enqueued è NULL o se l'idClient è un valore negativo\n
+ *              ENOENT se il pathname non è presente nello storage
+ * 
  * @return 0 in caso di successo, -1 altrimenti
  */
 int S_lockFile(storage_t* cache, char* pathname, int idClient, int* enqueued);
@@ -99,6 +118,10 @@ int S_lockFile(storage_t* cache, char* pathname, int idClient, int* enqueued);
  * @param pathname Pathname del file da unlockare
  * @param idClient FD del client che rilascia la lock
  * @param newLocker FD del client che adesso detiene la lock
+ * 
+ * @exception   EINVAL se pathname è NULL, se newLocker è NULL o se l'idClient è un valore negativo\n
+ *              EPERM se chi richiede la unlock non deteneva la lock sul file\n
+ *              ENOENT se il pathname non è presente nello storage
  *
  * @return 0 in caso di successo, -1 altrimenti
  */
@@ -110,12 +133,15 @@ int S_unlockFile(storage_t* cache, char* pathname, int idClient, int* newLocker)
  * @param cache Puntatore allo storage
  * @param pathname Pathname del file da eliminare
  * @param idClient FD del client che richiede la rimozione
- * @param stopLock FD dei client in attesa della lock
- * @param maxfd FD max in stopLock
+ * @param lockWaiters Coda dei file descriptor dei client che erano in attesa della lock sul file eliminato
+ * 
+ * @exception   EINVAL se pathname è NULL, se lockWaiters è NULL o se l'idClient è un valore negativo\n
+ *              EPERM se il client non ha la lock e il file è lockato
+ *              ENOENT se il pathname non è presente nello storage
  *
  * @return La coda dei client da avvisare, -1 altrimenti
  */
-int S_removeFile(storage_t* cache, char* pathname, int idClient, fd_set* stopLock, int* maxfd);
+int S_removeFile(storage_t* cache, char* pathname, int idClient, intqueue_t* lockWaiters);
 
 /**
  * @brief Crea un nuovo file nello storage NON INCREMENTA IL CONTATORE DEI FILE
@@ -123,6 +149,10 @@ int S_removeFile(storage_t* cache, char* pathname, int idClient, fd_set* stopLoc
  * @param cache Puntatore allo storage
  * @param pathname Pathname del file da aggiungere allo storage
  * @param idClient FD del client che crea il file
+ * 
+ * @exception   EINVAL se pathname è NULL o se l'idClient è un valore negativo\n
+ *              EEXIST se il file era già presente in cache\n
+ *              ENOMEM se fallisce l'allocazione di memoria
  * 
  * @return 0 in caso di successo, -1 altrimenti
  */
@@ -134,6 +164,9 @@ int S_createFile(storage_t* cache, char* pathname, int idClient);
  * @param cache Puntatore allo storage
  * @param pathname Pathname del file da aprire
  * @param idClient FD del client che ha accesso al file
+ * 
+ * @exception   EINVAL se pathname è NULL o se l'idClient è un valore negativo\n
+ *              ENOENT se il pathname non è presente nello storage
  *
  * @return 0 in caso di successo, -1 altrimenti
  */
@@ -144,6 +177,9 @@ int S_openFile(storage_t* cache, char* pathname, int idClient);
  * 
  * @param cache Puntatore allo storage
  * @param pathname Pathname del file da chiudere
+ * 
+ * @exception   EINVAL se pathname è NULL o se l'idClient è un valore negativo\n
+ *              ENOENT se il pathname non è presente nello storage
  *
  * @return 0 in caso di successo, -1 altrimenti
  */
@@ -158,11 +194,29 @@ int S_closeFile(storage_t* cache, char*pathname);
  * @param idClient FD del client che richiede la lettura
  * @param buf Contenuto del file
  * @param size Dimensione del contenuto
- * @param read Bytes letti
+ * 
+ * @exception   EINVAL se pathname è NULL o se l'idClient è un valore negativo\n
+ *              ENOENT se il pathname non è presente nello storage\n
+ *              EPERM se il file è bloccato da un client che non è idClient, oppure se idClient non ha aperto il file
  *
- * @return Il numero dei bytes letti, -1 in caso di errore
+ * @return 0 in caso di successo, -1 altrimenti
  */
-int S_readFile(storage_t* cache, char* pathname, int idClient, void** buf, size_t* size, int* read);
+int S_readFile(storage_t* cache, char* pathname, int idClient, void** buf, size_t* size);
+
+/**
+ * @brief Legge al più n file (o tutti se n è un intero minore o uguale a zero)
+ *        e li mette nella coda passata dal server, anche se sono chiusi o bloccati
+ * 
+ * @param cache Puntatore allo storage
+ * @param idClient FD del client che richiede la lettura
+ * @param n Numero di file da leggere (se <= 0 vengono letti tutti i file presenti)
+ * @param fileRead Coda che conterrà i file letti
+ * 
+ * @exception EINVAL se fileRead è NULL, se fileRead è NULL o se l'idClient è un valore negativo
+ * 
+ * @return 0 in caso di successo, -1 altrimenti
+ */
+int S_readNFiles(storage_t* cache, int idClient, int n, Queue_t* fileRead);
 
 /**
  * @brief Carica effettivamente il file nello storage INCREMENTA IL CONTATORE DEI FILE
@@ -175,11 +229,15 @@ int S_readFile(storage_t* cache, char* pathname, int idClient, void** buf, size_
  * @param content Contenuto da caricare nel file
  * @param size Dimesione del contenuto da scrivere
  * @param victim Coda di file espulsi
- * @param written Bytes scritti
+ * 
+ * @exception   EINVAL se pathname è NULL, se victim è NULL, se content è NULL, se size è un valore negativo o se l'idClient è un valore negativo\n
+ *              ENOENT se il file non è stato ancora creato (non è presente in cache)\n
+ *              EPERM se il file è bloccato da un client che non è idClient, se openFile(OCREAT) non è stata l'operazione precedente oppure se idClient non ha aperto il file\n
+ *              EFBIG se il file è troppo grande
  * 
  * @return 0 in caso di successo, -1 altrimenti
  */
-int S_uploadFile(storage_t* cache, char* pathname, int idClient, void* content, size_t size, Queue_t* victim, int* written);
+int S_uploadFile(storage_t* cache, char* pathname, int idClient, void* content, size_t size, Queue_t* victim);
 
 /**
  * @brief Scrive in append sul file indicato il contenuto passato.
@@ -192,11 +250,15 @@ int S_uploadFile(storage_t* cache, char* pathname, int idClient, void* content, 
  * @param victim Coda di file espulsi
  * @param content Contenuto da scrivere in append
  * @param size Dimesione del contenuto da scrivere
- * @param written Bytes scritti
+ * 
+ * @exception   EINVAL se pathname è NULL, se victim è NULL, se content è NULL, se size è un valore negativo o se l'idClient è un valore negativo\n
+ *              ENOENT se il file non è presente in cache\n
+ *              EPERM se il file è bloccato da un client che non è idClient, oppure se idClient non ha aperto il file\n
+ *              EFBIG se il file è troppo grande
  *
  * @return 0 in caso di successo, -1 altrimenti
  */
-int S_appendFile(storage_t* cache, char* pathname, int idClient, Queue_t* victim, void* content, size_t size, int* written);
+int S_appendFile(storage_t* cache, char* pathname, int idClient, Queue_t* victim, void* content, size_t size);
 
 /**
  * @brief Esegue il rimpiazzamento, non fa la free dei file e li pusha in victim,
@@ -207,8 +269,10 @@ int S_appendFile(storage_t* cache, char* pathname, int idClient, Queue_t* victim
  * @param victim Coda dei file espulsi
  * @param size Minimo spazio che dovrà essere libero nello storage al termine della funzione
  * 
- * @return 0 in caso di successo, -1 altrimenti 
+ * @exception EIDRM se durante l'esecuzione viene eliminato il file pathname (solo caso APPEND)
+ * 
+ * @return 0 in caso di successo, -1 altrimenti
  */
-int cacheMiss(storage_t* cache, Queue_t* victim, size_t size);
+int cacheMiss(storage_t* cache, Queue_t* victim, size_t size, char* pathname);
 
 #endif //STORAGE_H
